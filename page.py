12ef59ebd3dec391e306e5234868dd639c3771e9
Deivid Braian <deividbraian5@gmail.com>
2024-03-30 17:00:17 -0300

    adiciona csv de exemplo

--- page.py
+++ page.py
@@ -3,6 +3,9 @@ import pandas as pd
 from solution import Solver
 
 st.sidebar.header("Insira um arquivo csv com os nomes dos participantes")
+st.sidebar.markdown("""
+[Exemplo de arquivo CSV](https://raw.githubusercontent.com/braiansmarzaro/otimizador-de-mesas/main/match.csv)
+""")
 uploaded_file = st.sidebar.file_uploader("Upload CSV", help="Insira um arquivo csv onde a primeira coluna tem o nome do estudante e as colunas seguintes são suas preferências. O csv deve conter o título das colunas",type="csv")
 
 st.header("Organizador de mesas para estudantes e empresários")
